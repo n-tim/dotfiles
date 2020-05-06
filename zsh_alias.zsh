3522cd0f0974ef08192feeab9773bfc597957e45
timur_office <timur.code@gmail.com>
2020-05-06 20:45:15 +0200

    linux mbcopy alias

--- zsh_alias.zsh
+++ zsh_alias.zsh
@@ -1,7 +1,13 @@
 alias go="git checkout"
 alias st="git st"
 alias tmux-start='tmux start-server; tmux attach'
+alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 
 if [[ $OSTYPE == darwin* ]]; then
     alias vim='/usr/local/Cellar/vim/8.2.0550/bin/vim'
+fi
+
+if [[ $OSTYPE == linux* ]]; then
+    alias pbcopy='xclip -selection c'
+    alias pbpaste='xclip -selection clipboard -o'
 fi
