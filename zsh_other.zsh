406c624ba6a0d02e3efa4e3e4311cba7f16a75ab
tim <timur.code@gmail.com>
2022-02-01 23:50:25 +0100

    remove poweline, use nord theme for tmux

--- zsh_other.zsh
+++ zsh_other.zsh
@@ -1,24 +1,19 @@
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:$PATH
 
-ZSH_THEME="philips"
+#ZSH_THEME="jaischeema"
+ZSH_THEME="jbergantine"
 
 setopt nosharehistory
 bindkey '^R' history-incremental-search-backward
 
 if [[ $OSTYPE == darwin* ]]; then
-    pythonUserPackagesDir=$(python -m site --user-site)
-    if [[ -r $pythonUserPackagesDir/powerline/bindings/zsh/powerline.zsh ]]; then
-      source $pythonUserPackagesDir/powerline/bindings/zsh/powerline.zsh
-    fi
 else  if [[ $OSTYPE == linux* ]]; then
         if [[ -r /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-          source /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
+            source /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
         fi
-      fi
+    fi
 fi
-
-POWERLEVEL9K_COLOR_SCHEME='light'
 
 export EDITOR=vim
 export VISUAL=vim
