dad9766c68952ad30b1d4ed51a4689fc04505bfa
tim <timur.code@gmail.com>
2022-03-01 23:57:28 +0100

    remove vim binding

--- zsh_alias.zsh
+++ zsh_alias.zsh
@@ -3,9 +3,9 @@ alias st="git st"
 alias tmux-start='tmux start-server; tmux attach'
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 
-if [[ $OSTYPE == darwin* ]]; then
-    alias vim='/usr/local/Cellar/vim/8.2.0550/bin/vim'
-fi
+# if [[ $OSTYPE == darwin* ]]; then
+#     alias vim='/usr/local/Cellar/vim/8.2.0550/bin/vim'
+# fi
 
 if [[ $OSTYPE == linux* ]]; then
     alias pbcopy='xclip -selection c'
